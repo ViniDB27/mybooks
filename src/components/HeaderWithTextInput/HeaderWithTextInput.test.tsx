a428358f85af94f3db6cf4ea54bce074d998b855
Vinicio Brejinski <vinicio@MacBook-Air-de-Vinicio.local>
2022-03-31 19:15:22 -0300

    feat: create tests by HeaderWithTextInput

--- src/components/HeaderWithTextInput/HeaderWithTextInput.test.tsx
+++ src/components/HeaderWithTextInput/HeaderWithTextInput.test.tsx
@@ -1,13 +1,25 @@
-import render from "react-test-renderer";
+import render, { ReactTestRendererJSON } from "react-test-renderer";
 
 import { HeaderWithTextInput } from "./index";
 
 describe("<HeaderWithTextInput />", () => {
   const tree = render
     .create(<HeaderWithTextInput value="Mock" onChangeText={() => {}} />)
-    .toJSON();
+    .toJSON() as ReactTestRendererJSON;
 
-  it("renders correctly", () => {
-    expect(tree).toMatchSnapshot();
-  });
+    it("verify if component is not null ", () => {
+      expect(tree).not.toEqual(null);
+    });
+  
+    it("verify if have 1 child", () => {
+      expect(tree.children?.length).toEqual(1);
+    });
+  
+    it("renders correctly", () => {
+      expect(tree).toMatchSnapshot();
+    });
+    
+    it("verify if type is View", () => {
+      expect(tree.type).toEqual("View");
+    });
 });
